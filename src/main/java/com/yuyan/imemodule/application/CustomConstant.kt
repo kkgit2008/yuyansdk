77a3b75c558bae69ce6f8d4542701eabc1c66101
gure <gurecn@163.com>
2025-06-06 09:48:52 +0800

    fix(ime):升级词库版本

--- src/main/java/com/yuyan/imemodule/application/CustomConstant.kt
+++ src/main/java/com/yuyan/imemodule/application/CustomConstant.kt
@@ -14,7 +14,7 @@ object CustomConstant {
     const val SCHEMA_ZH_DOUBLE_FLYPY = "double_pinyin_" // 小鹤双拼
     const val SCHEMA_ZH_DOUBLE_LX17 = "double_pinyin_ls17" // 乱序17双拼
     const val SCHEMA_ZH_STROKE = "stroke" // 五笔画
-    const val CURRENT_RIME_DICT_DATA_VERSIOM = 20250520
+    const val CURRENT_RIME_DICT_DATA_VERSIOM = 20250606
     const val YUYAN_IME_REPO = "https://github.com/gurecn/YuyanIme"
     const val YUYAN_IME_REPO_GITEE = "https://gitee.com/gurecn/YuyanIme"
     const val LICENSE_URL = "https://www.gnu.org/licenses/gpl-3.0.html"
